814b07b5f606baaa7f7d2fe15c2823d395a0e4c8
Promgirl65 <107438673+Promgirl65@users.noreply.github.com>
2025-10-07 16:46:38 +0100

    Add PillProps import to Pill component

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,3 +1,5 @@
+import { PillProps } from "@/interfaces";
+
 const Pill: React.FC = () => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
@@ -7,3 +9,4 @@ const Pill: React.FC = () => {
 }
 
 export default Pill;
+
