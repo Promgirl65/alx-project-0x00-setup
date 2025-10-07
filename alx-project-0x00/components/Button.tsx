5c1f5f0bc68cd34de1688b3357705f5d25a39427
Promgirl65 <107438673+Promgirl65@users.noreply.github.com>
2025-10-07 16:53:36 +0100

    Add Button component with props and styles

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import { ButtonProps } from '../interfaces';
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button className={`px-4 py-2 font-semibold text-white bg-blue-500 hover:bg-blue-600 transition ${styles}`}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
 
